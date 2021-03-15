5b6acb177af2ef69d8f3659966b0f1b83f9faf4f
Ruzil Sharifullin <ruzil.sharifullin.ravilevich@gmail.com>
2021-03-14 20:17:02 -0700

    Add meal entity

--- src/main/java/sharifullinruzil/crudapp/domain/Meal.java
+++ src/main/java/sharifullinruzil/crudapp/domain/Meal.java
@@ -1,2 +1,52 @@
-package sharifullinruzil.crudapp.domain;public class Meal {
+package sharifullinruzil.crudapp.domain;
+
+import java.time.LocalDateTime;
+
+public class Meal {
+    Integer id;
+    LocalDateTime localDateTime;
+    String description;
+    Integer calories;
+
+    public Meal() {
+    }
+
+    public Meal(Integer id, LocalDateTime localDateTime, String description, Integer calories) {
+        this.id = id;
+        this.localDateTime = localDateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
 }
