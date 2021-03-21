c8e7cdf2f81ef0b9ce79f8526699dab9496e8289
Ruzil Sharifullin <ruzil.sharifullin.ravilevich@gmail.com>
2021-03-21 13:46:46 -0700

    Add NotFoundException for hanlding query results of persistence layer

---
+++ src/main/java/sharifullinruzil/crudapp/util/NotFoundException.java
@@ -0,0 +1,9 @@
+package sharifullinruzil.crudapp.util;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
