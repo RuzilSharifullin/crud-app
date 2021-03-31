6ed92bfe00bfad5881409c171196240b8c9acee9
Ruzil Sharifullin <ruzil.sharifullin.ravilevich@gmail.com>
2021-03-31 03:37:18 -0700

    Add jackson object mapper to customize json serialization

--- src/main/java/sharifullinruzil/crudapp/config/MvcConfig.java
+++ src/main/java/sharifullinruzil/crudapp/config/MvcConfig.java
@@ -1,11 +1,43 @@
 package sharifullinruzil.crudapp.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("sharifullinruzil.crudapp.web")
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.dateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm"));
+        builder.visibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        builder.visibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        return builder.build();
+    }
+
+
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        return new MappingJackson2HttpMessageConverter(objectMapper());
+    }
+
+    @Override
+    public void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
+
 }
